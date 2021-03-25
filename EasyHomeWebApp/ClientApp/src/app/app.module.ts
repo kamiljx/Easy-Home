c9358c7993f24529f6669b2ae127e9bc7f1cbd68
Kamil Rzeszutek <48318419+kamiljx@users.noreply.github.com>
2021-03-25 01:51:52 +0100

    web -> registerForm

--- EasyHomeWebApp/ClientApp/src/app/app.module.ts
+++ EasyHomeWebApp/ClientApp/src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { TextFormInputComponent } from './forms/text-form-input/text-form-input.component';
+import { DateFormInputComponent } from './forms/date-form-input/date-form-input.component';
+import { SharedModule } from './shared/shared.module';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    TextFormInputComponent,
+    DateFormInputComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot([
-    ])
+    ]),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
