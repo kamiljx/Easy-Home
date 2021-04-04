02dac1e71b3d3cd6996a2d860e536e2fb0c08bce
Kamil Rzeszutek <48318419+kamiljx@users.noreply.github.com>
2021-04-05 01:27:58 +0200

    added ngx-translate

--- EasyHomeWebApp/ClientApp/src/app/app.component.ts
+++ EasyHomeWebApp/ClientApp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private translateService: TranslateService){
+    this.translateService.setDefaultLang('pl');
+    this.translateService.use(localStorage.getItem('lang') || 'pl');
+  }
 }
--- EasyHomeWebApp/ClientApp/src/app/app.module.ts
+++ EasyHomeWebApp/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -10,8 +10,15 @@ import { RegisterComponent } from './register/register.component';
 import { TextFormInputComponent } from './forms/text-form-input/text-form-input.component';
 import { DateFormInputComponent } from './forms/date-form-input/date-form-input.component';
 import { SharedModule } from './shared/shared.module';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { AppRoutingModule } from './app-routing.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
   declarations: [
@@ -29,9 +36,18 @@ import { AppRoutingModule } from './app-routing.module';
     SharedModule,
     RouterModule.forRoot([
     ]),
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  }),
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
