907ce1c05c6a8a56dc856c2268ec4e03bb43bd7c
Dymion <damianskorupa4@gmail.com>
2021-04-25 00:36:45 +0200

    Add AdminControler and CreateRole method

---
+++ EasyHomeWebApp/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models.DataSource.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EasyHomeWebApp.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpPost("createRole")]
+        public async Task<IActionResult> CreateRole([FromBody] object roleName)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(roleName.ToString());
+            if (!roleExist)
+            {
+                var role = new AppRole { Name = roleName.ToString() };
+                await _roleManager.CreateAsync(role);
+                return Ok();
+            }
+            return Conflict();
+        }
+    }
+
+}
