e1ab7ab7527789f1788f0fd68440a9d1f2ec52ab
Dymion <damianskorupa4@gmail.com>
2021-04-26 18:22:28 +0200

    Update RealEstate

--- Models/DataSource/Entities/RealEstate.cs
+++ Models/DataSource/Entities/RealEstate.cs
@@ -12,19 +12,22 @@ namespace Models.DataSource.Entities
         [Required]
         public string OwnerId { get; set; }
         [Required]
+        public string Name { get; set; }
+        [Required]
         public string City { get; set; }
         [Required]
         public string ZipCode { get; set; }
         [Required]
-        public string StreetName { get; set; }
-        [Required]
-        public string HouseNumber { get; set; }
+        public string Address { get; set; }
+
         [Required]
         public string Country { get; set; }
         // Access code will be generated on creation of real estate
         public Guid AccessCode { get; set; }
         // Tu powinna być relacja one-to-many (jedna nieruchomość - wiele płatności). Sprawdzić czy napewno to się tak definiuje
         public IEnumerable<Payment> Payments { get; set; }
+
+        public ApplicationUser[] Rentiers { get; set; }
         
         
                
