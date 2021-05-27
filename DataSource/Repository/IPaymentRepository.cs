3a3b26020aa238836c7ef372601a513d36a561b5
Sebastian Rajtar <sebastian.rajtar@microsoft.wsei.edu.pl>
2021-05-27 15:38:27 +0200

    Repositories for payments and identity

---
+++ DataSource/Repository/IPaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models.DataSource.Entities;
+using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSource.Repository
+{
+    public interface IPaymentRepository
+    {
+        void AddPaymentToRealEstate(Payment payment);
+        Payment GetPaymentForRealEstate(int realEstateId);
+        void ModifyPayment(Payment payment);
+    }
+}
---
+++ DataSource/Repository/PaymentRepository.cs
@@ -0,0 +1,43 @@
+﻿using Models.DataSource.Entities;
+using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataSource.Repository
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public PaymentRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void AddPaymentToRealEstate(Payment payment)
+        {
+            dbContext.Payments.Add(payment);
+            dbContext.SaveChanges();
+        }
+
+        public Payment GetPaymentForRealEstate(int realEstateId)
+        {
+            Payment payment = dbContext.Payments.FirstOrDefault(p => p.RealEstateId == realEstateId);
+
+            return payment;
+        }
+
+        public void ModifyPayment(Payment payment)
+        {
+            Payment _payment = dbContext.Payments.FirstOrDefault(p => p.Id == payment.Id);
+
+            if (_payment != null)
+            {
+                _payment = payment;
+            }
+
+            dbContext.SaveChanges();
+        }
+    }
+}
