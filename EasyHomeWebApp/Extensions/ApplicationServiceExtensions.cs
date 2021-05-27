62129b66234077fa7aa51958e3f551fd5ef1c782
Sebastian Rajtar <sebastian.rajtar@microsoft.wsei.edu.pl>
2021-05-27 15:39:48 +0200

    Unneccesary usings and white spaces removed

--- EasyHomeWebApp/Extensions/ApplicationServiceExtensions.cs
+++ EasyHomeWebApp/Extensions/ApplicationServiceExtensions.cs
@@ -4,10 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Models.Interfaces;
 using Services.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EasyHomeWebApp.Extensions
 {
